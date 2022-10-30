2b2a47e631e59bf2d570b3f36eb15e59991881ed
aishahrazak <nuraishahrazak@yahoo.com>
2022-10-30 17:43:12 +0800

    responsive layout

--- src/index.js
+++ src/index.js
@@ -6,25 +6,25 @@ import shipData from './shiplayout.json'
 class Square extends React.Component {
     render() {
         return (
-            <button id={this.props.value} key={this.props.value} className="square" onClick={function() {}}>
+            <button id={this.props.value} key={this.props.value} className="square" onClick={function () { }}>
                 {/* TODO */}
             </button>
         );
     }
 }
 
-class ScoreBoard extends React.Component{
+class ScoreBoard extends React.Component {
     render() {
         return (
-            <div>
+            <div className="scoreBoard">
                 <div className="scoreTile player1">
                     <div className="score">00</div>
-                    <hr className="scoreLine"/>
+                    <hr className="scoreLine" />
                     <div className="player">player 1</div>
                 </div>
                 <div className="scoreTile player2">
                     <div className="score">00</div>
-                    <hr className="scoreLine"/>
+                    <hr className="scoreLine" />
                     <div className="player">player 2</div>
                 </div>
             </div>
@@ -32,28 +32,28 @@ class ScoreBoard extends React.Component{
     }
 }
 
-class ShipInfo extends React.Component{
-    renderShip(shipType, count){
-        var shipImgsDict ={
-            battleship : "assets/Battleship Shape.png",
-            carrier : "assets/Carrier Shape.png",
-            submarine : "assets/Submarine Shape.png",
-            cruiser : "assets/Cruiser Shape.png",
-            destroyer : "assets/Aircraft Shape.png",
+class ShipInfo extends React.Component {
+    renderShip(shipType, count) {
+        var shipImgsDict = {
+            battleship: "assets/Battleship Shape.png",
+            carrier: "assets/Aircraft Shape.png",
+            submarine: "assets/Submarine Shape.png",
+            cruiser: "assets/Cruiser Shape.png",
+            destroyer: "assets/Carrier Shape.png",
         }
         let imgPath = shipImgsDict[shipType]
         let size = "o ".repeat(count)
         return (
             <div key={shipType} className="ship">
-                <img className="shipType" src={imgPath} alt={shipType}/>
+                <img className="shipType" src={imgPath} alt={shipType} />
                 <div className="shipCount">{size}</div>
             </div>
         )
     }
 
-    render(){
+    render() {
         let shipInfo = []
-        for(let ship in shipData.shipTypes){
+        for (let ship in shipData.shipTypes) {
             console.log(ship + " : " + shipData.shipTypes[ship].size)
             shipInfo.push(this.renderShip(ship, shipData.shipTypes[ship].size))
         }
@@ -67,23 +67,23 @@ class ShipInfo extends React.Component{
 
 class Board extends React.Component {
     renderSquare(i) {
-        return <Square value={i}/>;
+        return <Square value={i} />;
     }
 
     render() {
         var board = []
-        for(let i = 0; i < 10; i++){
-                let squares = []
-                for(let j = 0; j < 10; j++){
-                    squares.push(this.renderSquare(i + "," + j))
-                }
-                let rowId = "row" + i ;
-                let element = (
-                    <div className="board-row" key={rowId}>
-                        {squares}
-                    </div>
-                )
-                board.push(element)
+        for (let i = 0; i < 10; i++) {
+            let squares = []
+            for (let j = 0; j < 10; j++) {
+                squares.push(this.renderSquare(i + "," + j))
+            }
+            let rowId = "row" + i;
+            let element = (
+                <div className="board-row" key={rowId}>
+                    {squares}
+                </div>
+            )
+            board.push(element)
         }
         return (
             <div className="game-board">
@@ -94,14 +94,30 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+    renderHeader(){
+        return (
+            <div className="header">
+                <button className="menuBtn"/>
+                <div className="searchBar"/>
+                <button className="searchBtn"/>
+                <button className="appBtn"/>
+                <button className="appBtn"/>
+                <button className="appBtn"/>
+            </div>
+        );
+    }
+
     render() {
         return (
-            <div className="game">
-                <div className="game-info">
-                    <ScoreBoard />
+            <div>
+                {this.renderHeader()}
+                <div className="game">
+                    <Board />
+                    <div className="game-info">
+                        <ScoreBoard />
+                        <ShipInfo />
+                    </div>
                 </div>
-                <ShipInfo />
-                <Board />
             </div>
         );
     }
